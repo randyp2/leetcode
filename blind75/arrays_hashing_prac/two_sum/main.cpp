8a9f697f9065ca83c7fdd05e84232d982c9f4f8d
Randy Pahang II <rpahang2@gmail.com>
2026-03-18 22:53:17 -0700

    i use arch btw

--- blind75/arrays_hashing_prac/two_sum/main.cpp
+++ blind75/arrays_hashing_prac/two_sum/main.cpp
@@ -3,7 +3,7 @@
 #include <unordered_map>
 #include <vector>
 
-class Soluton {
+class Solution {
   public:
     std::vector<int> twoSum(std::vector<int> &nums, int target) {
         const int N = nums.size();
@@ -15,13 +15,32 @@ class Soluton {
 
             if (m.find(diff) != m.end())
                 return {m[diff], i};
+
             m[curr_num] = i;
         }
+
         return {};
+
+        throw std::runtime_error("Function not implemented...");
     }
 };
 
 int main() {
-    std::cout << "--- TWO SUM ---\n";
+    std::cout << "---TWO SUM---\n";
+
+    Solution sol;
+
+    std::vector<int> test1 = {2, 7, 11, 15};
+    std::vector<int> test2 = {3, 2, 4};
+    std::vector<int> test3 = {3, 3};
+
+    std::cout << "Test 1: " << (sol.twoSum(test1, 9))[0] << ", "
+              << (sol.twoSum(test1, 9))[1] << std::endl;
+    std::cout << "Test 2: " << (sol.twoSum(test2, 6))[0] << ", "
+              << (sol.twoSum(test2, 6))[1] << std::endl;
+    std::cout << "Test 3: " << (sol.twoSum(test3, 6))[0] << ", "
+              << (sol.twoSum(test3, 3))[1] << std::endl;
+
+    return 0;
     return 0;
 }

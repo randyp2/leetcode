a75caceeee38b5ad7050225c2197863bf06321f8
Randy Pahang II <rpahang2@gmail.com>
2026-04-27 01:28:38 -0700

    two summmmm

---
+++ blind75/arrays_hashing/two_sum/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+
+  public:
+    std::vector<int> twoSum(std::vector<int> &nums, int target) {
+
+        std::unordered_map<int, int> m; // maps number: index
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int curr_num = nums[i];
+            int diff = target - curr_num;
+
+            // If diff exists in map
+            if (m.find(diff) != m.end())
+                return {m[diff], i};
+
+            m[curr_num] = i;
+        }
+
+        return {};
+    }
+};
+
+int main() {
+    std::cout << "--- TWO SUM ---\n";
+    return 0;
+};

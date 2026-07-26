6f4b849aeae90b2852c591a5548dc6911076d301
Randy Pahang II <rpahang2@gmail.com>
2026-07-25 20:10:32 -0700

    find minimum in rotated binary array

---
+++ blind75/binary_search/find_min_in_rotated_array/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+
+  public:
+    int findMin(std::vector<int> &nums) {
+        if (nums.size() == 1)
+            return nums[0];
+
+        int l = 0;
+        int r = nums.size();
+
+        while (l < r) {
+            int m = l + (r - l) / 2;
+
+            // Array is rotated search answer is in the right half
+            if (nums[m] > nums[r]) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+
+        return nums[l];
+    }
+};
+
+int main() {
+    std::cout << "--- FIND MINIMUM IN ROTATED ARRAY ---\n";
+    return 0;
+};
---
+++ blind75/binary_search/find_min_in_rotated_array/notes.md
@@ -0,0 +1,9 @@
+# TOPIC
+- Binary search we can tell because:
+ - Requires logn runtime (binary search?)
+ - Fits the narative because array is somewhat sorted 
+
+- Essentially two sorted arrays
+- Answer is the left most element between those two sorted arrays
+
+
